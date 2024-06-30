ad3c105b4f5e9c1a10565c022e3d41252f6f9681
ninditobing7 <ninditobing7@gmail.com>
2024-07-01 00:53:29 +0700

    perbaiki codingan rusak

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,12 @@ function App() {
     },
   ])
 
-  // console.log(todos)
-  const toggleCompleted = (todoId) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === todoId) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    })
-    setTodos(updatedTodos)
-  }
+  console.log(todos)
   
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos } toggleCompleted={toggleCompleted}/>
+      <Todos todos={todos} />
     </div>
   )
 }
